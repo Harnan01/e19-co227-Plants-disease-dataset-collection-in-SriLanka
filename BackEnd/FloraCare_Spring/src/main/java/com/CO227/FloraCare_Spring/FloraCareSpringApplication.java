b08261a5fda5334fabebd6173225d1dd3de6ee58
Harnan <115539697+Harnan01@users.noreply.github.com>
2023-09-29 16:25:42 +0530

    Springboot project fole created and updated

---
+++ BackEnd/FloraCare_Spring/src/main/java/com/CO227/FloraCare_Spring/FloraCareSpringApplication.java
@@ -0,0 +1,13 @@
+package com.CO227.FloraCare_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloraCareSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloraCareSpringApplication.class, args);
+	}
+
+}
---
+++ BackEnd/FloraCare_Spring/src/test/java/com/CO227/FloraCare_Spring/FloraCareSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CO227.FloraCare_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloraCareSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
