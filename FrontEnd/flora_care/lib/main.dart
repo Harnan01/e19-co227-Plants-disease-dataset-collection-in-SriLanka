705a11fa373fcb324a3d44d8b8050d5f8fff6dea
Harnan <115539697+Harnan01@users.noreply.github.com>
2023-10-04 14:16:21 +0530

    Updated main.dart with Hive Integration for Token Storage and Conditional Navigation to Login or Home Page

--- FrontEnd/flora_care/lib/main.dart
+++ FrontEnd/flora_care/lib/main.dart
@@ -3,13 +3,29 @@ import 'package:connectivity/connectivity.dart';
 import 'nointernet.dart';
 import 'login.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'homepage.dart';
+import 'HiveBoxes.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Loading(),
-  )
-);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Loading(),
+    ),
+  );
+}
+
+
+// Define the _handleLogout function here in main.dart
+
+
+
 
 class Loading extends StatefulWidget {
   @override
@@ -26,20 +42,42 @@ class _LoadingState extends State<Loading> {
   }
 
   Future<void> _checkInternetAndNavigate() async {
-    final connectivityResult = await Connectivity().checkConnectivity();
-    if (connectivityResult == ConnectivityResult.none) {
-      // No internet connection, navigate to the no internet page
+  final connectivityResult = await Connectivity().checkConnectivity();
+  if (connectivityResult == ConnectivityResult.none) {
+    // No internet connection, navigate to the no internet page
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+      builder: (context) => NoInternet(),
+    ));
+  } else {
+    // Check if a token exists in Hive
+    final token = await getTokenFromHive(); // Implement this function
+
+    if (token != null && token.isNotEmpty) {
+      // Token exists, navigate to the home page
       Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (context) => NoInternet(),
+        builder: (context) => MyApp(), // Replace with your home page widget
       ));
     } else {
-      // Internet connection available, navigate to the login page
+      // No token, navigate to the login page
       Navigator.of(context).pushReplacement(MaterialPageRoute(
         builder: (context) => LogIn(),
       ));
-      
     }
   }
+}
+
+// Function to check for the token in Hive
+Future<String?> getTokenFromHive() async {
+  try {
+    final box = await Hive.openBox<String>(HiveBoxes.tokenBox);
+    final token = box.get('token');
+    return token;
+  } catch (e) {
+    // Handle any potential errors, e.g., when Hive is not initialized or the box doesn't exist
+    return null;
+  }
+}
+
 
   @override
   Widget build(BuildContext context) {
